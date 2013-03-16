f0209161547c872007bf892ed6b293e0a62e9dd2
Lucas Caton <lucascaton@gmail.com>
2013-03-15 21:25:49 -0300

    Including SettingsLogic + HTTP basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  protected
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == Settings.authentication.username && password == Settings.authentication.password
+    end
+  end
 end
