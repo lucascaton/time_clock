819fa61b87d253df4167670ac0da7f35ad9e2266
Lucas Caton <lucascaton@gmail.com>
2013-03-15 21:02:20 -0300

    Including TimeConverter class

---
+++ app/models/time_converter.rb
@@ -0,0 +1,37 @@
+class TimeConverter
+  def initialize(seconds)
+    @seconds = seconds
+  end
+
+  # Code adapted from:
+  # http://stufftohelpyouout.blogspot.com.br/2010/02/seconds-to-days-minutes-hours-seconds.html
+  def to_time
+    days    = @seconds / 86400
+    hours   = (@seconds / 3600) - (days * 24)
+    minutes = (@seconds / 60) - (hours * 60) - (days * 1440)
+    seconds = @seconds % 60
+
+    display, display_concat = '', ''
+
+    if days > 0
+      display = display + display_concat + "#{days} day#{days == 1 ? '' : 's'}"
+      display_concat = ', '
+    end
+
+    if hours > 0
+      display = display + display_concat + "#{hours} hour#{hours == 1 ? '' : 's'}"
+      display_concat = ', '
+    end
+
+    if minutes > 0
+      display = display + display_concat + "#{minutes} minute#{minutes == 1 ? '' : 's'}"
+      display_concat = ', '
+    end
+
+    if seconds > 0
+      display = display + display_concat + "#{seconds} second#{seconds == 1 ? '' : 's'}"
+    end
+
+    display
+  end
+end
---
+++ spec/models/time_converter_spec.rb
@@ -0,0 +1,35 @@
+require File.expand_path('../../../app/models/time_converter', __FILE__)
+
+describe TimeConverter, focus: true do
+  describe '#to_time' do
+    it 'returns a formatted time' do
+      seconds = 1 # 1 second
+      time_converted = TimeConverter.new(seconds).to_time
+      expect(time_converted).to eql('1 second')
+    end
+
+    it 'returns a formatted time' do
+      seconds = 60 + 2 # 1.minute + 2.seconds
+      time_converted = TimeConverter.new(seconds).to_time
+      expect(time_converted).to eql('1 minute, 2 seconds')
+    end
+
+    it 'returns a formatted time' do
+      seconds = 3600 + 120 + 3 # 1.hour + 2.minutes + 3.seconds
+      time_converted = TimeConverter.new(seconds).to_time
+      expect(time_converted).to eql('1 hour, 2 minutes, 3 seconds')
+    end
+
+    it 'returns a formatted time' do
+      seconds = 86_400 + 7200 + 180 + 4 # 1.day + 2.hours + 3.minutes + 4.seconds
+      time_converted = TimeConverter.new(seconds).to_time
+      expect(time_converted).to eql('1 day, 2 hours, 3 minutes, 4 seconds')
+    end
+
+    it 'returns a formatted time' do
+      seconds = 2_592_000 + 172_800 + 10800 + 240 + 5 # 1.month + 2.days + 3.hours + 4.minutes + 5.seconds
+      time_converted = TimeConverter.new(seconds).to_time
+      expect(time_converted).to eql('32 days, 3 hours, 4 minutes, 5 seconds')
+    end
+  end
+end
